02e8e03a0e43d9fbcfa3e8d774f2b557a35e7adc
Erijl <justusmau@web.de>
2024-12-02 18:26:04 +0100

    solve part 2 day 02

--- src/main/java/org/erijl/aoc/Day02.java
+++ src/main/java/org/erijl/aoc/Day02.java
@@ -19,34 +19,64 @@ public class Day02 {
             reports.add(Arrays.stream(levels).map(Integer::parseInt).collect(Collectors.toList()));
         }
 
-        int safeReportCount = 0;
+        int safeReportCountPartOne = 0;
         for (List<Integer> report : reports) {
-            boolean increasing = false;
-            boolean decreasing = false;
-            boolean isSame = false;
-            boolean toMuchDif = false;
-
-            for (int i = 1; i < report.size(); i++) {
-                if(report.get(i) < report.get(i-1)) decreasing = true;
-
-                if(report.get(i) > report.get(i-1)) increasing = true;
-
-                if(Objects.equals(report.get(i), report.get(i - 1))) isSame = true;
-
-                if(Math.abs((report.get(i) - report.get(i-1))) > 3) toMuchDif = true;
-            }
-
-            if(!(increasing && decreasing) && !isSame && !toMuchDif) {
-                safeReportCount++;
-            }
+            boolean isReportSafe = isReportSafe(report);
+            if (isReportSafe) safeReportCountPartOne++;
         }
 
-        System.out.printf("Safe reports: %d", safeReportCount);
+        int safeReportCount = 0;
+        for (List<Integer> report : reports) {
+            boolean isReportSafe = isReportSafe(report);
+            if (isReportSafe) {
+                safeReportCount++;
+            } else {
+                boolean isAnySafe = false;
+                List<Integer> baseReport = new ArrayList<>(report);
+
+                for (int i = 0; i < report.size(); i++) {
+                    report.remove(i);
+                    boolean isPartialReportSafe = isReportSafe(report);
+                    assert (baseReport.size() - 1) == report.size();
+
+                    if (isPartialReportSafe) {
+                        isAnySafe = true;
+                        break;
+                    }
+                    report = new ArrayList<>(baseReport);
+                }
+
+                if (isAnySafe) safeReportCount++;
+            }
+
+
+        }
+
+        System.out.printf("Safe reports (Part One): %d, Safe reports with problem dampener: %d", safeReportCountPartOne, safeReportCount);
+    }
+
+    private static boolean isReportSafe(List<Integer> report) {
+        boolean increasing = false;
+        boolean decreasing = false;
+        boolean isSame = false;
+        boolean toMuchDif = false;
+
+        for (int i = 1; i < report.size(); i++) {
+            if (report.get(i) < report.get(i - 1)) decreasing = true;
+
+            if (report.get(i) > report.get(i - 1)) increasing = true;
+
+            if (Objects.equals(report.get(i), report.get(i - 1))) isSame = true;
+
+            if (Math.abs((report.get(i) - report.get(i - 1))) > 3) toMuchDif = true;
+        }
+
+        return !(increasing && decreasing) && !isSame && !toMuchDif;
     }
 
 
     private static String getRawInput() {
-        try(BufferedReader br = new BufferedReader(new FileReader(Day01.class.getClassLoader().getResource("input-day-02.txt").getPath()))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(Day01.class.getClassLoader().getResource("input-day-02.txt").getPath()))) {
             StringBuilder sb = new StringBuilder();
             String line = br.readLine();
 
