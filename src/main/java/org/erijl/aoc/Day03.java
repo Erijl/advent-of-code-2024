652f885b9dc5e8863465c9f740e5953a173a0b67
Erijl <justusmau@web.de>
2024-12-03 21:39:53 +0100

    solve part 2 day 03

--- src/main/java/org/erijl/aoc/Day03.java
+++ src/main/java/org/erijl/aoc/Day03.java
@@ -11,8 +11,51 @@ import java.util.regex.Pattern;
 
 public class Day03 {
 
+    private static final String DO_STRING = "do()";
+    private static final String DONT_STRING = "don't()";
+
     public static void main(String[] args) {
         String input = getRawInput();
+
+
+        BigInteger product = evaluteOnlyMulInstructions(input);
+        BigInteger productPartTwo = evaluteMulDosAndDontsInstructions(input);
+
+        System.out.printf("Product (Part One): %d, Product (Part Two): %d", product, productPartTwo);
+    }
+
+    public static BigInteger evaluteMulDosAndDontsInstructions(String input) {
+        List<String> matches = new ArrayList<>();
+
+        Matcher multiplicationMatcher = Pattern.compile("(mul\\(\\d+,\\d+\\))|do\\(\\)|don't\\(\\)").matcher(input);
+        while(multiplicationMatcher.find()) {
+            matches.add(multiplicationMatcher.group());
+        }
+
+        matches.forEach(System.out::println);
+        boolean isMultiplicationEnabled = true;
+        BigInteger product = BigInteger.ZERO;
+        for (String match : matches) {
+            if (match.equals(DO_STRING)) isMultiplicationEnabled = true;
+            if (match.equals(DONT_STRING)) isMultiplicationEnabled = false;
+
+
+            if(match.startsWith("mul") && isMultiplicationEnabled) {
+                String[] numbers = match.replace("mul(", "").replace(")", "").split(",");
+                product = product.add(
+                        new BigInteger(numbers[0])
+                                .multiply(
+                                        new BigInteger(numbers[1]
+                                        )
+                                )
+                );
+            }
+        }
+
+        return product;
+    }
+
+    public static BigInteger evaluteOnlyMulInstructions(String input) {
         List<String> matches = new ArrayList<>();
 
         Matcher multiplicationMatcher = Pattern.compile("mul\\(\\d+,\\d+\\)").matcher(input);
@@ -34,7 +77,7 @@ public class Day03 {
             );
         }
 
-        System.out.printf("Product: %d", product);
+        return product;
     }
 
     private static String getRawInput() {
